8fd4e89f0b0be500b4beb2a011fceac60a73c17f
Junhyup Lee <jlee0124@uni.sydney.edu.au>
2025-03-16 00:25:03 +1100

    Implemented real-time ASCII cube rendering with rotation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include <cmath>
+#include <string>
+#include <unistd.h>
 
 float A, B, C;
+
+float cubeWidth = 10;
+int width = 160, height = 44;
+float zBuffer[160 * 44];
+char buffer[160 * 44];
+int backgroundASCIICode = ' ';
+int distanceFromCam = 60;
+float K1 = 40;
+
+float increamentSpeed = 0.6;
+
+float x,y,z;
+float ooz;
+int xp, yp;
+int idx;
 
 float calculateX(int i, int j, int k) {
     return j * std::sin(A) * std::sin(B) * std::cos(C) - k * std::cos(A) * std::sin(B) * std::cos(C) +
@@ -20,9 +37,40 @@ float calculateZ(int i, int j, int k) {
             i * std::sin(B);
 }
 
+void calculateForSurface(float cubeX, float cubeY, float cubeZ, int character) {
+    x = calculateX(cubeX, cubeY, cubeZ);
+    y = calculateY(cubeX, cubeY, cubeZ);
+    z = calculateZ(cubeX, cubeY, cubeZ) + distanceFromCam;
+    ooz = 1/z;
+    xp = (int)(width/2 + K1 * ooz * x * 2);
+    yp = (int)(height/2 + K1 * ooz * y);
+
+    idx = xp + yp * width;
+    if(idx >= 0 && idx <  width * height) {
+        if(ooz > zBuffer[idx]) {
+            zBuffer[idx] = ooz;
+            buffer[idx] = character;
+        }
+    }
+}
+
 int main() {
+    printf("\x1b[2J");
+    while(true) {
+        memset(buffer, backgroundASCIICode, width * height);
+        memset(zBuffer, 0, width * height * 4);
+        for(float cubeX = -cubeWidth ; cubeX < cubeWidth ; cubeX += increamentSpeed) {
+            for(float cubeY = -cubeWidth ; cubeY < cubeWidth ; cubeY += increamentSpeed) {
+                calculateForSurface(cubeX, cubeY, -cubeWidth, '#');
+            }
+        }
+        printf("\x1b[H");
+        for(int k = 0 ; k < width * height ; k++) {
+            putchar(k % width ? buffer[k] : 10);
+        }
 
-    std::cout << "Hello World" << std::endl;
-
+        A += 0.005;
+        B += 0.005;
+    }
     return 0;
 }
