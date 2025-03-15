b68ec16a28dbd5e7a49ae62ff64679415d0602c3
Junhyup Lee <jlee0124@uni.sydney.edu.au>
2025-03-15 17:24:58 +1100

    Added functions to calculte  X Y Z

--- main.cpp
+++ main.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
+#include <cmath>
+
+float A, B, C;
+
+float calculateX(int i, int j, int k) {
+    return j * std::sin(A) * std::sin(B) * std::cos(C) - k * std::cos(A) * std::sin(B) * std::cos(C) +
+            j * std::cos(A) * std::sin(C) + k * std::sin(A) * std::sin(C) +
+            i * std::cos(B) * std::cos(C);
+}
+
+float calculateY(int i, int j, int k) {
+    return j * std::cos(A) * std::cos(C) + k * std::sin(A) * std::cos(C) -
+            j * std::sin(A) * std::sin(B) * std::sin(C) + k * std::cos(A) * std::sin(B) * std::sin(C) -
+            i * std::cos(B) * std::sin(C);
+}
+
+float calculateZ(int i, int j, int k) {
+    return k * std::cos(A) * std::cos(B) - j * std::sin(A) * std::cos(B) +
+            i * std::sin(B);
+}
 
 int main() {
 
